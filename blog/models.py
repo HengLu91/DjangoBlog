103d90e46e8940d6142cbdd02b1cd316bbcd59f3
HengLu91 <1058691147@qq.com>
2018-04-24 01:23:26 +0800

    增加博客详情页

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -26,4 +27,6 @@ class Post(models.Model):
     author = models.ForeignKey(User,on_delete=models.CASCADE)
     def __str__(self):
         return self.title
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
 
